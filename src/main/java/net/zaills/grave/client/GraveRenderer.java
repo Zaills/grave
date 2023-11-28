bcadfc510f73143424d44501a693847ed529c898
Zaills <39807152+Zaills@users.noreply.github.com>
2023-11-28 10:48:35 +0100

    skull good

--- src/main/java/net/zaills/grave/client/GraveRenderer.java
+++ src/main/java/net/zaills/grave/client/GraveRenderer.java
@@ -1,81 +1,70 @@
 package net.zaills.grave.client;
 
-import net.minecraft.block.SkullBlock;
 import net.minecraft.client.MinecraftClient;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
-import net.minecraft.client.render.block.entity.SkullBlockEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.SkullEntityModel;
+import net.minecraft.client.util.DefaultSkinHelper;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.math.*;
+import net.minecraft.util.Identifier;
 import net.zaills.grave.block.entity.GraveBlockEntity;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public class GraveRenderer implements BlockEntityRenderer<GraveBlockEntity> {
+	//public static final Identifier GRAVE = new Identifier("grave", "grave");
+
+	private final TextRenderer Tr;
+	private final SkullEntityModel PhM;
+	private final SkullEntityModel ShM;
+
+
+	public static final Identifier SkT = new Identifier("textures/entity/skeleton/skeleton.png");
+	public GraveRenderer(BlockEntityRendererFactory.Context ctx){
+		this.Tr = ctx.getTextRenderer();
+		this.PhM = new SkullEntityModel(ctx.getLayerRenderDispatcher().getModelPart(EntityModelLayers.PLAYER_HEAD));
+		this.ShM = new SkullEntityModel(ctx.getLayerRenderDispatcher().getModelPart(EntityModelLayers.SKELETON_SKULL));
+	}
+
 	@Override
 	public void render(GraveBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-		Direction direction = entity.getCachedState().get(Properties.HORIZONTAL_FACING);
-
 		matrices.push();
-
-		matrices.scale(1, 1, 1);
-
-		switch (direction){
+		matrices.translate(.5, 0, .5);
+		matrices.scale(-1, -1, -1);
+		var model = entity.getOwner() == null ? this.ShM : this.PhM;
+		switch (entity.getCachedState().get(Properties.HORIZONTAL_FACING)){
 			case NORTH:
-				SkullBlockEntityRenderer.renderSkull(null, 0f, 0f, matrices, vertexConsumers, light, SkullBlockEntityRenderer.getModels(MinecraftClient.getInstance().getEntityModelLoader()).get(SkullBlock.Type.PLAYER), SkullBlockEntityRenderer.getRenderLayer(SkullBlock.Type.PLAYER, entity.getOwner()));
+				model.setHeadRotation(0, 180, 0);
 				break;
 			case SOUTH:
-				SkullBlockEntityRenderer.renderSkull(null, 180f, 0f, matrices, vertexConsumers, light, SkullBlockEntityRenderer.getModels(MinecraftClient.getInstance().getEntityModelLoader()).get(SkullBlock.Type.PLAYER), SkullBlockEntityRenderer.getRenderLayer(SkullBlock.Type.PLAYER, entity.getOwner()));
+				model.setHeadRotation(0, 0, 0);
 				break;
 			case EAST:
-				SkullBlockEntityRenderer.renderSkull(null, 90f, 0f, matrices, vertexConsumers, light, SkullBlockEntityRenderer.getModels(MinecraftClient.getInstance().getEntityModelLoader()).get(SkullBlock.Type.PLAYER), SkullBlockEntityRenderer.getRenderLayer(SkullBlock.Type.PLAYER, entity.getOwner()));
+				model.setHeadRotation(0, 90, 0);
 				break;
 			case WEST:
-				SkullBlockEntityRenderer.renderSkull(null, -90f, 0f, matrices, vertexConsumers, light, SkullBlockEntityRenderer.getModels(MinecraftClient.getInstance().getEntityModelLoader()).get(SkullBlock.Type.PLAYER), SkullBlockEntityRenderer.getRenderLayer(SkullBlock.Type.PLAYER, entity.getOwner()));
+				model.setHeadRotation(0, 270, 0);
 				break;
 		}
-
+		model.render(matrices, vertexConsumers.getBuffer(getRL(entity.getOwner())), light, overlay, 1, 1, 1, 1);
 		matrices.pop();
+	}
 
-		String name;
-		if (entity.getOwner() != null) {
-			name = entity.getOwner().getName();
+	public static RenderLayer getRL(@Nullable GameProfile Pf){
+		if (Pf != null){
+			MinecraftClient client = MinecraftClient.getInstance();
+			Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = client.getSkinProvider().getTextures(Pf);
+			return map.containsKey(MinecraftProfileTexture.Type.SKIN) ? RenderLayer.getEntityTranslucent(client.getSkinProvider().loadSkin(map.get(MinecraftProfileTexture.Type.SKIN), MinecraftProfileTexture.Type.SKIN)) : RenderLayer.getEntityTranslucent(DefaultSkinHelper.getTexture(Pf.getId()));
 		}
-		else{
-			name = "NoName";
-		}
-		matrices.push();
-
-		int wd = MinecraftClient.getInstance().textRenderer.getWidth(name);
-
-		float scl = .7f / wd;
-
-
-		matrices.translate(.5, .6, .5);
-		Entity cam = MinecraftClient.getInstance().cameraEntity;
-		assert cam != null;
-		matrices.multiply(new Quaternion(0, 1, 0, getYaw(entity.getPos(), cam.getPos())));
-		matrices.scale(1, -1, 0);
-
-		matrices.scale(scl, scl, scl);
-		matrices.translate(-wd / 2., -4.5, 0);
-		MinecraftClient.getInstance().textRenderer.draw(name, 0, 0, 0xFFFFFF, true, matrices.peek().getModel(), vertexConsumers, true, 0, light);
-		matrices.pop();
-
+		return RenderLayer.getEntityCutoutNoCullZOffset(SkT);
 	}
 
-	static float getYaw(BlockPos Bpos, Position Ppos){
-		return (float) Math.atan2(Bpos.getY(), Ppos.getY());
-	}
-
-	public GraveRenderer(BlockEntityRenderDispatcher dispatch){
-		this.dispatch = dispatch;
-	}
-	public BlockEntityRenderDispatcher dispatch;
-
-	public GraveRenderer(BlockEntityRendererFactory.Context context){
-		this.dispatch = context.getRenderDispatcher();
-	}
 }
