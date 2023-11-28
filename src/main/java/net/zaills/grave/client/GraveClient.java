af316d8901054f507fa81b5a98ae04cbca054211
Zaills <39807152+Zaills@users.noreply.github.com>
2023-11-28 15:35:56 +0100

    clean code

--- src/main/java/net/zaills/grave/client/GraveClient.java
+++ src/main/java/net/zaills/grave/client/GraveClient.java
@@ -1,15 +1,10 @@
 package net.zaills.grave.client;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
-import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.zaills.grave.Grave;
-import net.zaills.grave.block.entity.GraveBlockEntity;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
-@Environment(EnvType.CLIENT)
 public class GraveClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
