1c16435124b6371697976c785c3a09b48357ed08
DownloadableFox <me@downloadablefox.dev>
2024-03-04 16:02:02 -0500

    fixed block placement function

--- src/main/java/net/zaills/grave/Grave.java
+++ src/main/java/net/zaills/grave/Grave.java
@@ -21,6 +21,8 @@ import net.zaills.grave.block.GraveBlock;
 import net.zaills.grave.block.Grave_notype;
 import net.zaills.grave.block.entity.GraveBlockEntity;
 import net.zaills.grave.config.GraveConfig;
+
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor.Block.BlockType;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.block.entity.api.QuiltBlockEntityTypeBuilder;
@@ -28,10 +30,11 @@ import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class Grave implements ModInitializer {
 
-	public static  final GraveBlock GRAVE = new Grave_notype(QuiltBlockSettings.of(Material.DECORATION).strength(0.8f, -1f));
+	public static final GraveBlock GRAVE = new Grave_notype(QuiltBlockSettings.of(Material.DECORATION).strength(0.8f, -1f));
 	public static BlockEntityType<GraveBlockEntity> GRAVE_ENTITY;
 
 	public static final GraveConfig CONFIG = GraveConfig.createAndLoad();
@@ -43,72 +46,99 @@ public class Grave implements ModInitializer {
 		GRAVE_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "grave:grave", QuiltBlockEntityTypeBuilder.create(GraveBlockEntity::new, GRAVE).build(null));
 	}
 
-	public static void Place(World world, Vec3d pos, PlayerEntity player){
-		if (world.isClient)
+	public static void Place(World world, Vec3d pos, PlayerEntity player) {
+		if (world.isClient) {
 			return;
-
-		BlockPos bp = new BlockPos(pos.x, pos.y - 1, pos.z);
-
-		if (pos.y - 1 <= world.getDimension().minY()) {
-			bp = new BlockPos(new BlockPos(pos.x, world.getDimension().minY(), pos.z));
 		}
+
+		final int worldMin = world.getDimension().minY();
+		final int worldMax = world.getDimension().height() - worldMin;
+
+		// Create a default grave state for placing
+		BlockState graveState = GRAVE
+			.getDefaultState()
+			.with(Properties.HORIZONTAL_FACING, player.getHorizontalFacing());
+
+		BlockPos originBlock = new BlockPos(pos.x, (pos.y < worldMin) ? worldMin : pos.y, pos.z);
 
 		DefaultedList<ItemStack> inv = DefaultedList.of();
 		inv.addAll(player.getInventory().main);
 		inv.addAll(player.getInventory().armor);
 		inv.addAll(player.getInventory().offHand);
-		for (Pair<SlotReference, ItemStack> pair : TrinketsApi.getTrinketComponent(player).get().getAllEquipped())
+		
+		for (Pair<SlotReference, ItemStack> pair : TrinketsApi.getTrinketComponent(player).get().getAllEquipped()) {
 			inv.add(pair.getRight());
-
-		boolean placed = false;
-
-		for (BlockPos gP : BlockPos.iterateOutwards(bp.add(new Vec3i(0, 1, 0)), 5, 5, 5)){
-			if (bp.getY() > world.getDimension().minY() || bp.getY() < world.getDimension().height() - world.getDimension().minY()){
-				BlockState gS = GRAVE.getDefaultState().with(Properties.HORIZONTAL_FACING, player.getHorizontalFacing());
-
-				gP = place_top(gP, world);
-				placed = world.setBlockState(gP, gS);
-				GraveBlockEntity graveBlockEntity = new GraveBlockEntity(gP, gS);
-				graveBlockEntity.setInv(inv);
-				graveBlockEntity.setOwner(player.getGameProfile());
-				graveBlockEntity.setXp(player.totalExperience);
-				graveBlockEntity.setLocation(gP);
-				graveBlockEntity.setType(get_type(world.getBlockState(new BlockPos(pos)).getBlock()));
-				world.addBlockEntity(graveBlockEntity);
-
-				graveBlockEntity.markDirty();
-				world.getBlockState(bp).getBlock().onBreak(world, bp, world.getBlockState(bp), player);
-
-				player.totalExperience = 0;
-				player.experienceLevel = 0;
-				player.experienceProgress = 0;
-
-				System.out.println(player.getName() + "'s grave spawn at: " + gP.getX() + ", " + gP.getY() + ", " + gP.getZ());
-				if (CONFIG.Get_grave_coord())
-					player.sendMessage(Text.of("Grave spawn at: " + gP.getX() + ", " + gP.getY() + ", " + gP.getZ()), false);
-
-				break;
-			}
 		}
 
-		if (!placed)
-			player.getInventory().dropAll();
+		for (BlockPos offsetBlock : BlockPos.iterateOutwards(originBlock, 5, 5, 5)) {
+			// Not within world bounds, then skip
+			if (worldMin > offsetBlock.getY() || offsetBlock.getY() > worldMax) {
+				continue;
+			}
+
+			// Check to see if the block is within the world border
+			if (!world.getWorldBorder().contains(offsetBlock)) {
+				continue;
+			}
+
+			// Look for air block to place on from offset
+			Optional<BlockPos> airBlock = place_top(offsetBlock, world);
+
+			// If no valid position found, then skip offset
+			if (airBlock.isEmpty()) {
+				continue;
+			}
+
+			// Get the placing block
+			BlockPos placingBlock = airBlock.get();
+
+			// Attempt to place block
+			if (!world.setBlockState(placingBlock, graveState)) {
+				continue;
+			}
+
+			// Set block data 
+			GraveBlockEntity graveBlockEntity = new GraveBlockEntity(placingBlock, graveState);
+			graveBlockEntity.setInv(inv);
+			graveBlockEntity.setOwner(player.getGameProfile());
+			graveBlockEntity.setXp(player.totalExperience);
+			graveBlockEntity.setLocation(placingBlock);
+			graveBlockEntity.setType(get_type(world.getBlockState(placingBlock).getBlock()));
+			graveBlockEntity.markDirty();
+			
+			world.addBlockEntity(graveBlockEntity);
+
+			System.out.println(player.getName() + "'s grave spawn at: " + placingBlock.getX() + ", " + placingBlock.getY() + ", " + placingBlock.getZ());
+			if (CONFIG.Get_grave_coord()) {
+				player.sendMessage(Text.of("Grave spawn at: " + placingBlock.getX() + ", " + placingBlock.getY() + ", " + placingBlock.getZ()), false);
+			}
+
+			return;
+		}
+
+		// Unable to place grave enywhere (skill issue), drop items
+		player.getInventory().dropAll();
 	}
 
 	static int get_type(Block bl){
 		if (!Objects.equals(bl.getName().getString(), "Air")){
 			return 1;
 		}
+
 		return 0;
 	}
 
-	static BlockPos place_top(BlockPos pos, World world){
+	static Optional<BlockPos> place_top(BlockPos pos, World world) {
 		BlockPos npos = pos;
-		while(!Objects.equals(world.getBlockState(new BlockPos(npos)).getBlock().getName().getString(), "Air")){
-			npos = new BlockPos(npos.getX(), npos.getY() + 1, npos.getZ());
-			if (npos.getY() > world.getDimension().height())
-				return pos;
+
+		while (world.getBlockState(npos).getMaterial() != Material.AIR) {
+			npos = npos.add(0, 1, 0);
+
+			if (npos.getY() > world.getDimension().height()) {
+				return Optional.empty();
+			}
 		}
-		return npos;
+
+		return Optional.of(npos);
 	}
 }
